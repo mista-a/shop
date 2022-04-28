2aa2fcf47009697441e65147075cfa1eb281d5ce
mista-a <dvaanddva@gmail.com>
2022-04-28 08:59:50 +0300

    create ProductCard page

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -1,11 +1,12 @@
 import { Card, CardMedia, Typography } from '@mui/material'
-import Link from 'next/link'
+import Link from '../UI/Link/Link'
 import React, { FC, useState } from 'react'
 import cardImg from '../../assets/images/card-img.png'
 import styles from './Product.module.sass'
 import { ReactComponent as LikeIcon } from '../../assets/images/like-icon.svg'
 
 interface IProduct {
+  id: number
   img: string
   name: string
   colors: string[]
@@ -22,7 +23,13 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
   return (
     <div className={styles.card}>
       <div className={styles.imgWrapper}>
-        <img className={styles.cardImg} src={product.img} alt={product.name} />
+        <Link href={`/product/${product.id}`}>
+          <img
+            className={styles.cardImg}
+            src={product.img}
+            alt={product.name}
+          />
+        </Link>
         <button
           className={styles.likeButton}
           onClick={() => setInFavorite(!inFavorite)}
